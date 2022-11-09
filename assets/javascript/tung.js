8bfdb65656a04edc990bfcb6a3f38ed27b783f3c
TungPT12 <mylifeundermycontrol@gmail.com>
2022-11-09 23:13:09 +0700

    fix UI

--- assets/javascript/tung.js
+++ assets/javascript/tung.js
@@ -1,34 +1,41 @@
 let audio = document.querySelector("#audio");
+console.log(audio);
 let img = document.querySelector('.header_img');
 let pause = document.querySelector(".fa-pause-circle");
 let play = document.querySelector(".fa-play-circle");
 let nowPlayingSong = document.querySelector('.header_name--song')
-let music = [
+let musics = [
   {
     img: 'https://avatar-ex-swe.nixcdn.com/singer/avatar/2016/04/28/9/a/5/b/1461818138466.jpg',
     name: 'Sold Out',
-    song: 'SoldOut'
-
+    song: 'SoldOut',
+    singer: 'Hawk Nelson',
+    time: '3:29'
   },
   {
     img: 'https://i.ytimg.com/vi/4yI_sl_WvnU/maxresdefault.jpg',
     name: 'Unstoppable',
-    song: 'Unstoppable'
+    song: 'Unstoppable',
+    singer: 'Sia',
+    time: '4:11'
   },
   {
     img: 'https://image-us.24h.com.vn/upload/4-2022/images/2022-11-03/mono-4-1667456729-835-width660height444.jpg',
-    name: 'WaitingForYou',
-    song: 'Waiting For You'
+    name: 'Waiting For You',
+    song: 'WaitingForYou',
+    singer: 'Mono',
+    time: '4:25'
   },
   {
     img: 'https://i.ytimg.com/vi/red9YvYlPWg/maxresdefault.jpg',
-    name: 'Cuối_Cùng_Thì',
-    song: 'Cuối Cùng Thì'
+    name: 'Cuối Cùng Thì',
+    song: 'Cuối_Cùng_Thì',
+    singer: 'Jack',
+    time: '3:40'
   }
 ]
-var playSong = (classSong, imgLink, nameSong, nameSongPlaying) => {
-  let elementNameSong = document.querySelector(`.${classSong}`)
-  elementNameSong.onclick = () => {
+var playSong = (track, imgLink, nameSong, nameSongPlaying) => {
+    track.onclick = () => {
     nowPlayingSong.innerHTML = nameSongPlaying
     img.style.animationPlayState = 'running';
     img.src = imgLink;
@@ -39,10 +46,41 @@ var playSong = (classSong, imgLink, nameSong, nameSongPlaying) => {
   }
 }
 
-playSong('track1', 'https://avatar-ex-swe.nixcdn.com/singer/avatar/2016/04/28/9/a/5/b/1461818138466.jpg', 'SoldOut', 'Sold Out')
-playSong('track2', 'https://i.ytimg.com/vi/4yI_sl_WvnU/maxresdefault.jpg', 'Unstoppable', 'Unstoppable')
-playSong('track3', 'https://image-us.24h.com.vn/upload/4-2022/images/2022-11-03/mono-4-1667456729-835-width660height444.jpg', 'WaitingForYou', 'Waiting For You')
-playSong('track4', 'https://i.ytimg.com/vi/red9YvYlPWg/maxresdefault.jpg', 'Cuối_Cùng_Thì', 'Cuối Cùng Thì')
+var queryList = (element) => {
+  return document.querySelectorAll(`.${element}`)
+}
+
+let eventListSong = () => {
+  let listSong = queryList('track');
+  listSong.forEach((track, index) => {
+    playSong(track, musics[index].img, musics[index].song, musics[index].name)
+
+    
+  });
+}
+
+var renderSong = () => {
+  let listSong = musics.map((music) => {
+    return `<div class="track" >
+                <img src="${music.img}" alt="" class="track__img">
+                <div class="track__infor">
+                    <p class="track__infor-song">${music.name}</p>
+                    <p class="track__infor-singer">${music.singer}</p>
+                </div>
+                <p class="track__time">4:13</p>
+            </div>`
+  });
+  let elements = listSong.join('');
+  let wrapperElement = document.querySelector("#wrapper");
+  wrapperElement.innerHTML = elements
+
+  eventListSong();
+}
+renderSong()
+
+
+
+
 
 
 
